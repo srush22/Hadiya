ba9e6dd186965abeea135b088fb6fee03d96a094
Srushti Pagare <53164582+srush22@users.noreply.github.com>
2024-10-25 16:40:16 +0530

    Update app.component.ts

--- hadiya_products_frontend/src/app/app.component.ts
+++ hadiya_products_frontend/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+selector: 'app-root',
+templateUrl: './app.component.html',
+styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'hadiya_products_admin';
+export class AppComponent implements OnInit {
+title = 'hadiya_products_admin';
+constructor(private http: HttpClient) {}
+ngOnInit() {
+this.http.get('http://52.37.86.186:3000').subscribe((data: any) => {
+console.log(data);
+});
+}
 }
